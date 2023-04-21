1288b52d5c1797c56c4ca96db75ccf292a854933
Angel Junior Curtido Quiñonez <106999344+angeljrcurtido@users.noreply.github.com>
2023-04-21 11:33:54 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Portafolio from './components/portafolio';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <Portafolio/>
+       
+    </div>
+  );
+}
+
+export default App;
