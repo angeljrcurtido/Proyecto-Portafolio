48273c173288e2e4037af5c389449ac15b87115e
Angel Junior Curtido Quiñonez <106999344+angeljrcurtido@users.noreply.github.com>
2023-04-21 11:29:07 -0400

    Add files via upload

---
+++ src/components/email.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import emailjs from 'emailjs-com';
+import './style.css'
+
+function Email() {
+  const [nombre, setNombre] = useState('');
+  const [email, setEmail] = useState('');
+  const [mensaje, setMensaje] = useState('');
+
+  function handleSubmit(evento) {
+    evento.preventDefault(); // Prevenimos el comportamiento por defecto del formulario
+
+    // Configuramos la información necesaria para enviar el correo con emailjs
+    const serviceID = 'service_zqr88u4'; // Reemplaza con tu servicio ID
+    const plantillaID = 'template_ftzgwkm'; // Reemplaza con tu plantilla ID
+    const publicKey = 'NFCEPlXLzUnhr14zo'; // Reemplaza con tu user ID
+
+    // Enviamos los datos del formulario usando emailjs
+    emailjs.send(serviceID, plantillaID, { to_name: nombre, from_name: email, message: mensaje }, publicKey)
+      .then(() => alert('El correo ha sido enviado correctamente.'))
+      .catch(error => console.log('Hubo un error al enviar el correo:', error));
+
+    // Limpiamos los campos del formulario
+    setNombre('');
+    setEmail('');
+    setMensaje('');
+  }
+  return (
+    <form className='formularioemail' onSubmit={handleSubmit}>
+
+      <div className='emails'>
+        <div className='misservicios'>
+          <h2>Correo</h2>
+        </div>
+        <label className="form-label" htmlFor="nombre">Nombre:</label>
+        <input className='form-control' type="text" id="nombre" value={nombre} onChange={evento => setNombre(evento.target.value)} />
+
+        <label className="form-label" htmlFor="email">Email:</label>
+        <input className='form-control' type="email" id="email" value={email} onChange={evento => setEmail(evento.target.value)} />
+
+        <label className="form-label" htmlFor="mensaje">Mensaje:</label>
+        <textarea className='form-control' id="mensaje" value={mensaje} onChange={evento => setMensaje(evento.target.value)}></textarea>
+
+        <button className='btn btn-success mt-1' type="submit">Enviar</button>
+      </div>
+      <div className='misservicios'>
+        <h2>Redes Sociales</h2>
+        <div id="social" className="aside-box">
+          <div className="twitter">
+            <a href="https://github.com/angeljrcurtido" target="_blank" class="icon">Q</a>
+            <p className="overlay">
+              Github
+            </p>
+          </div>
+          <div class="facebook">
+            <a href="https://www.facebook.com/junior.curtido" target="_blank" class="icon">f</a>
+            <p class="overlay">
+              Facebook
+            </p>
+          </div>
+          <div class="youtube">
+            <a href="https://www.linkedin.com/in/%E2%99%BE%EF%B8%8F-angel-junior-curtido-qui%C3%B1onez%E2%99%BE%EF%B8%8F-7a410323b" target="_blank" class="icon">l</a>
+            <p class="overlay">
+              Linkedin
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </form>
+
+  );
+}
+
+export default Email;
---
+++ src/components/portafolio.jsx
@@ -0,0 +1,279 @@
+import React from 'react';
+import { Document } from 'react-pdf/dist/esm/entry.webpack';
+import './style.css';
+import mongodblogo from '../imagenes/mongodblogo.png';
+import expresslogo from '../imagenes/expresslogo2.png';
+import logoreact from '../imagenes/logoreact.png';
+import logonode from '../imagenes/logonodejs.png';
+import logohtml from '../imagenes/logohtml.png';
+import logocss from '../imagenes/logocss.png';
+import logoboost from '../imagenes/logobootstratp.png';
+import rickandmorty from '../imagenes/RICKANDMORTY.PNG';
+import frontend from '../imagenes/FRONTENDWEB.PNG';
+import pelismedia from '../imagenes/PELISMEDIA.PNG'
+import Email from './email';
+import firefox from '../imagenes/firefox-icon.png';
+import opera from '../imagenes/Opera_256x256.png';
+import safari from '../imagenes/safari.png';
+import chrome from '../imagenes/chrome.png';
+import android from '../imagenes/android.png';
+import ios from '../imagenes/ios.png';
+import html5 from '../imagenes/logotiposfull.png';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function Portafolio() {
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const circlesContainer = document.querySelector('.logo-container');
+        let position = 0;
+
+        function animateCircles() {
+            position -= 70; // Ajusta esta medida según el tamaño de tus círculos y margen
+            if (position < -(circlesContainer.clientWidth)) {
+                position = 0;
+            }
+            circlesContainer.style.transform = `translateX(${position}px)`;
+        }
+
+        setInterval(animateCircles, 2000); // Ajusta el intervalo según la velocidad que quieras
+    });
+
+    return (
+
+        <div className='body' >
+            {/*INICIO CABECERA*/}
+            <header id="header">
+                <div class="wrap">
+                    <div id="logo">
+                        <span class="gear">S</span>
+                        <h3>PORTAFOLIO</h3>
+                    </div>
+                    <nav id="menu">
+                        <ul>
+                            <li><a href="#inicio">INICIO</a></li>
+                            <li><a href="#proyectos2">PROYECTOS</a></li>
+                            <li><a href="#contactos">CONTACTO</a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+            </header>
+            {/*FIN CABECERA*/}
+            {/*INICIO BANNER*/}
+            <div className='banner' id='inicio'>
+                <h1>PORTAFOLIO WEB ANGEL JR CURTIDO</h1>
+            </div>
+            {/*FIN BANNER*/}
+            {/*DESCRIPCION*/}
+            <p>¡Saludos! Soy un estudiante que se encuentra en el octavo
+                semestre de la carrera de Licenciatura de Analisis de Sistemas; actualmente,
+                me encuentro buscando una empresa con la cual pueda empezar mi
+                trayectoria profesional. Durante estos dos años, he estado aprendiendo
+                En cuanto al desarrollo web, he estado aprendiendo acerca de tecnologías
+                importantes como Mongoose, Express, y Node.js en conjunto con React.js.
+                De esta manera, estoy preparado para trabajar en proyectos web complejos
+                y colaborar con otros desarrolladores para crear soluciones innovadoras.
+                Además, he llevado a cabo proyecto tanto
+                en solitario como en conjunto con equipos.</p>
+            <Document file={require('../cv/mi_cv.pdf')} />
+            <div className='botondescarga'>
+                <a href={require('../cv/mi_cv.pdf')} download>Descargar mi CV</a>
+            </div>
+
+            {/*FIN DESCRIPCION*/}
+            {/*INICIO DE STACKS-SERVICIOS*/}
+            <div className='misservicios'>
+                <h2>Mis Servicios</h2>
+            </div>
+            {/*FIN DE STACKS-SERVICIOS*/}
+            <div className="subcaja2">
+
+                <div className="card">
+                    <p className='icon'>_</p>
+                    <h2 className="category"><strong>Website manageable</strong></h2>
+                    <p className="description">Poseo un conocimiento profundo en el flujo
+                        de trabajo que abarca desde el backend hasta el frontend en el
+                        desarrollo de aplicaciones </p>
+                </div>
+                <div className="card"> {/*Aqui se inserta la clase*/}
+                    <p className="icon">H</p>{/*Aqui el icono*/}
+                    <h2 className="category">Desarrollo Web</h2>{/*Titulo del card*/}
+                    <p className="description">{/*Aqui la descripcion del card*/}
+                        Creo completamente una estructura para el desarrollo del prototipo, me adapto a los problemas y nuevos retos con tal de llevar tus diseños a código
+                    </p>
+                </div>
+                <div className="card"> {/*Aqui se inserta la clase*/}
+                    <p className="icon">a</p>{/*Aqui el icono*/}
+                    <h2 className="category">Base de datos</h2>{/*Titulo del card*/}
+                    <p className="description">{/*Aqui la descripcion del card*/}
+                        Amplios conocimientos en manejo de Bases de datos experiencia en MongoDB y Sql
+                    </p>
+                </div>
+                <div className="card"> {/*Aqui se inserta la clase*/}
+                    <p className="icon">.</p>{/*Aqui el icono*/}
+                    <h2 className="category">Front-End</h2>{/*Titulo del card*/}
+                    <p className="description">{/*Aqui la descripcion del card*/}
+                        Desarrollo de diseños enfocados en lo requerimientos proporcionados
+                    </p>
+                </div>
+                <div className="card"> {/*Aqui se inserta la clase*/}
+                    <p className="icon">p</p>{/*Aqui el icono*/}
+                    <h2 className="category">Back-End</h2>{/*Titulo del card*/}
+                    <p className="description">{/*Aqui la descripcion del card*/}
+                        Implementación de backend a los diseños front-end proporcionados
+                    </p>
+                </div>
+            </div>
+
+            <div class="skills">
+
+                <div className='misservicios'>
+                    <h2>Mis Skills</h2>
+                </div>
+
+                <div>
+                    <Slider
+                        infinite={true}
+                        speed={500}
+                        autoplay={true}
+                        autoplaySpeed={3000}
+                        slidesToShow={6}
+                        slidesToScroll={1}>
+                        <div className='skill-logo'>
+                            <div className='logo-circle' >
+                                <img src={mongodblogo} alt="Logo de Mongodb" />
+                            </div>
+                            <p>MongoDB</p>
+                        </div>
+                        <div className='skill-logo'>
+                            <div className='logo-circle' >
+                                <img src={expresslogo} alt="Logo de Express js" />
+                            </div>
+                            <p>Express Js</p>
+                        </div>
+                        <div className='skill-logo'>
+                            <div className='logo-circle'>
+                                <img src={logoreact} alt="Logo de React js" />
+                            </div>
+                            <p>React Js</p>
+                        </div>
+                        <div className='skill-logo' >
+                            <div className='logo-circle'>
+                                <img src={logonode} alt="Logo de Node js" />
+                            </div>
+                            <p>Node Js</p>
+                        </div>
+                        <div className='skill-logo'>
+                            <div className='logo-circle'>
+                                <img src={logohtml} alt="Logo de Html" />
+                            </div>
+                            <p>Html</p>
+                        </div>
+                        <div className='skill-logo'>
+                            <div className='logo-circle'>
+                                <img src={logocss} alt="Logo de Css" />
+                            </div>
+                            <p>Css3</p>
+                        </div>
+                        <div className='skill-logo'>
+                            <div className='logo-circle'>
+                                <img src={logoboost} alt="Logo de Bootstratp" />
+                            </div>
+                            <p>Boostratp</p>
+                        </div>
+                    </Slider>
+                </div>
+
+            </div>
+
+            <div className='Proyectos' id='proyectos2'>
+                <div class="card" style={{ width: "18rem" }}>
+                    <img src={rickandmorty} class="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">APIRest RickandMorty</h5>
+                        <p className="card-title">React Js, APIRest, Boostratp,FlexBox</p>
+                        <a href="https://incandescent-entremet-8c4174.netlify.app" target='_blank' style={{ marginTop: "5%" }} className="btn btn-primary">Demo</a>
+                        <a href="https://github.com/angeljrcurtido/Rickandmorty" target='_blank' style={{ marginLeft: "5%", marginTop: "5%" }} className="btn btn-primary">Code</a>
+                    </div>
+                </div>
+                <div class="card" style={{ width: "18rem" }}>
+                    <img src={frontend} class="card-img-top" style={{ width: "286px", height: "145.72px" }} alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">FrontEnd Proyect</h5>
+                        <p className="card-title">CSS, Flexbox, Boostratp, Sass, Js</p>
+                        <a href="https://stellar-hotteok-29adaf.netlify.app" target='_blank' style={{ marginTop: "5%" }} className="btn btn-primary">Demo</a>
+                        <a href="https://github.com/angeljrcurtido/Maquetacion-FrontEnd" target='_blank' style={{ marginLeft: "5%", marginTop: "5%" }} className="btn btn-primary">Code</a>
+                    </div>
+                </div>
+                <div class="card" style={{ width: "18rem" }}>
+                    <img src={pelismedia} class="card-img-top" style={{ width: "286px", height: "145.72px" }} alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Pelis Media</h5>
+                        <p className="card-title">React Js, Node Js, Mongoose, Boostratp, JavaScript, </p>
+                        <a href="https://youtu.be/61hknSVA8dQ" target='_blank' style={{ marginTop: "5%" }} className="btn btn-primary">Video</a>
+                        <a href="https://github.com/angeljrcurtido/Proyecto-pelis-media.git" target='_blank' style={{ marginLeft: "5%", marginTop: "5%" }} className="btn btn-primary">Code</a>
+                    </div>
+                </div>
+            </div>
+            <div className='contactos'>
+                <div className='misservicios' id='contactos'>
+                    <h2>Contacto</h2>
+                </div>
+                <Email />
+            </div>
+            {/*INICIO DE PIE DE PAGINA */}
+            <div class="clearfix"></div>
+            <footer id="footer">
+                <div class="wrap2">
+                    <div id="menu_footer">
+                        <h5>MENÚ</h5>
+                        <ul>
+                            <li><a href="#inicio">INICIO</a></li>
+                            <li><a href="#proyectos2">PROYECTOS</a></li>          
+                            <li><a href="#contactos">CONTACTO</a></li>
+                        </ul>
+                    </div>
+                    <div id="info">
+                        <h5>Desarrollado con </h5>
+                        <p>
+                            <img src={html5}
+                                alt="Creado con Html y CSS" />
+                        </p>
+                        <h5>Optimizado para </h5>
+                        <p id="browsers">
+                            <a href="#">
+                                <img src={firefox} alt="Firefox" title="Firefox" />
+                            </a>
+                            <a href="#">
+                                <img src={chrome} alt="Chrome" title="Chrome" />
+                            </a>
+                            <a href="#">
+                                <img src={opera} alt="Opera" title="Opera" />
+                            </a>
+                            <a href="#">
+                                <img src={safari} alt="Safari" title="Safari" />
+                            </a>
+                            <a href="#">
+                                <img src={android} alt="Android" title="Android" />
+                            </a>
+                            <a href="#">
+                                <img src={ios} alt="Apple" title="Apple" />
+                            </a>
+
+                        </p>
+                        <h5>AUTOR </h5>
+                        <p>&copy; ANGEL CURTIDO WEB - Angel Junior Curtido</p>
+                    </div>
+                </div>
+            </footer>
+            {/*FIN DE PIE DE PAGINA */}
+        </div>
+
+    );
+
+}
+
+export default Portafolio;
